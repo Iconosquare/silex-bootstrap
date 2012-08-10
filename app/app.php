5ac2707827729cdcbff465954e6749f414498237
Sylvain Salomon <sylvain.salomon@tripnity.com>
2012-08-10 15:29:29 +0200

    ajout db

--- app/app.php
+++ app/app.php
@@ -3,8 +3,10 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
-
+$app->register(new \Tripnity\DbProvider\DbProvider());
+        
 $app->get('/', function() {
+  var_dump($app['db']->getConnection('lol'));
   $user = new \Tripnity\Model\User();
   return new \Symfony\Component\HttpFoundation\Response("Hello World");
 });
---
+++ src/Tripnity/Util/Db.php
@@ -0,0 +1,20 @@
+<?php
+namespace Tripnity\Util;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Db
+ *
+ * @author sylvain
+ */
+class Db {
+  //put your code here
+  
+  public function getConnection($string){
+    return 'say: '.$string;
+  }
+}
